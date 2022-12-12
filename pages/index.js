35371ab7f5e056a05ea2eb21581af9c07b35cb8f
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-12-12 11:25:03 +0530

    more ui

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='w3r-dot text-black'>
+      <div className='w3r-dot text-[#363f48]'>
         <div className='flex py-[3.125vw] px-[5vw] items-center text-[#363f48]'>
           <p className='text-[4.85vw] flex flex-col pr-[3.9vw] mr-[3.9vw] border-r-[0.15vw] border-[#a58838] font-bold'><span className='text-[#a58838] text-[1.875vw]'>LATEST</span>PROJECTS</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam nisi, sollicitudin venenatis nisl id, iaculis pharetra quam. Vivamus cursus est vel sagittis tristique. Nunc vel ligula mollis, imperdiet dolor quis, consectetur magna. Duis facilisis tempor lectus tempus dignissim. Praesent lacus ante, mattis sit amet purus non, suscipit pellentesque odio.</p>
@@ -75,7 +75,77 @@ export default function Home() {
           <p className='text-[4.85vw] flex flex-col pr-[3.9vw] mr-[3.9vw] border-r-[0.15vw] border-[#a58838] font-bold'><span className='text-[#a58838] text-[1.875vw]'>OUR</span>TEAM</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam nisi, sollicitudin venenatis nisl id, iaculis pharetra quam. Vivamus cursus est vel sagittis tristique. Nunc vel ligula mollis, imperdiet dolor quis, consectetur magna. Duis facilisis tempor lectus tempus dignissim. Praesent lacus ante, mattis sit amet purus non, suscipit pellentesque odio.</p>
         </div>
-        <div></div>
+        <div className='flex justify-center gap-[3vw]'>
+          <div className='w-[20.475vw] h-[50.2vh] shadow-lg bg-gray-400'>
+            <img src='' alt='employee' />
+            <div className='text-[1.57vw] font-bold flex justify-center p-[1.25vw] bg-white mt-[20vw] w-4/5 mx-auto shadow-lg flex-col items-center'>John Smith<span className='text-[1.25vw] font-normal'>designation</span></div>
+          </div>
+          <div className='w-[20.475vw] h-[50.2vh] shadow-lg bg-gray-400'>
+            <img src='' alt='employee' />
+            <div className='text-[1.57vw] font-bold flex justify-center p-[1.25vw] bg-white mt-[20vw] w-4/5 mx-auto shadow-lg flex-col items-center'>John Smith<span className='text-[1.25vw] font-normal'>designation</span></div>
+          </div>
+          <div className='w-[20.475vw] h-[50.2vh] shadow-lg bg-gray-400'>
+            <img src='' alt='employee' />
+            <div className='text-[1.57vw] font-bold flex justify-center p-[1.25vw] bg-white mt-[20vw] w-4/5 mx-auto shadow-lg flex-col items-center'>John Smith<span className='text-[1.25vw] font-normal'>designation</span></div>
+          </div>
+          <div className='w-[20.475vw] h-[50.2vh] shadow-lg bg-gray-400'>
+            <img src='' alt='employee' />
+            <div className='text-[1.57vw] font-bold flex justify-center p-[1.25vw] bg-white mt-[20vw] w-4/5 mx-auto shadow-lg flex-col items-center'>John Smith<span className='text-[1.25vw] font-normal'>designation</span></div>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ backgroundImage: `url("/banner.jpg")` }} className='w-full h-[20.32vw] bg-center bg-cover'>
+        <div className='w-full absolute h-[20.32vw]  bg-black opacity-60'></div>
+        <div className='flex w-full justify-evenly h-full items-center'>
+          <div className='flex flex-col z-10 font-bold items-center justify-center'>
+            <img className='w-[5vw]' src='https://img.icons8.com/sf-ultralight-filled/200/ffffff/home-page.png' alt="icon" />
+            <span className='text-[4.6875vw]'>1456</span>
+            <span>Projects Completed</span>
+          </div>
+          <div className='flex flex-col z-10 font-bold items-center justify-center'>
+            <img className='w-[5vw]' src='https://img.icons8.com/pastel-glyph/200/ffffff/loudspeaker--v2.png' alt="icon" />
+            <span className='text-[4.6875vw]'>432</span>
+            <span>Fresh Ideas</span>
+          </div>
+          <div className='flex flex-col z-10 font-bold items-center justify-center'>
+            <img className='w-[5vw]' src='https://img.icons8.com/material-sharp/200/ffffff/conference-call.png' alt="icon" />
+            <span className='text-[4.6875vw]'>992</span>
+            <span>Satisfied Clients</span>
+          </div>
+          <div className='flex flex-col z-10 font-bold items-center justify-center'>
+            <img className='w-[5vw]' src='https://img.icons8.com/material-rounded/200/ffffff/tea-cup.png' alt="icon" />
+            <span className='text-[4.6875vw]'>329</span>
+            <span>Cup of Coffee</span>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ backgroundImage: `url("/banner2.jpg")` }} className=' w-full font-bold text-[1.875vw] h-[24vw] flex-col flex justify-center items-center text-[#a58838]'>
+        TESTIMONIALS
+        <span className='text-[3.75vw] text-[#363f48]'>WHAT OUR CLIENT SAYS</span>
+      </div>
+
+      <div className='bg-white text-[#363f48] w-full h-[155vh]'>
+        <div className='w-full h-[42.5vw] flex'>
+          <div className='w-1/2 p-[4vw]'>
+            <div className='border-[#a58838] justify-center text-center flex flex-col  border-l-[0.2vw] border-b-[0.2vw] w-full h-full'>
+              <h1 className='text-[3.75vw]'>Quisque feugiat purus</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut scelerisque purus. Donec placerat tempor ullamcorper.</p>
+            </div>
+          </div>
+          <div className='w-1/2 h-full overflow-hidden'><img className='w-full h-full' src='/banner3.jpg' alt="testimonial" /></div>
+        </div>
+        <div className='flex '>
+          <div>
+            <img src='' alt='testimonial2' />
+            <span>Phasellus</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut scelerisque purus. Donec placerat tempor ullamcorper.
+          </div>
+          <div>
+            <img src='' alt='testimonial2' />
+            <span>Vestibulum</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut scelerisque purus. Donec placerat tempor ullamcorper.
+          </div>
+        </div>
       </div>
     </div>
   )
