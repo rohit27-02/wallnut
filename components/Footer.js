448ddd41b02a2461a412402cd06a112b6c348c76
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-12-12 17:19:46 +0530

    ui

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#191717]'>Footer</div>
+    <div className='bg-[#191717] w-full h-[50vh]'>
+      
+    </div>
   )
 }
 
