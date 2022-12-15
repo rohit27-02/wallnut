c3404f8db56c65b90c5f8c74eabf75f552125a92
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-12-15 10:57:58 +0530

    ui

--- pages/services.js
+++ pages/services.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import Banner from '../components/banner'
 
@@ -6,12 +7,54 @@ const services = () => {
     <div >
       <Banner title={"Services"}/>
       <div className="flex flex-wrap justify-center mb-[7vw] mt-[34vw]">
-        <div className='w-[28.1vw] bg-gray-300 h-[75.76vh] drop-shadow-md m-[1.5vw]'></div>
-        <div className='w-[28.1vw] bg-gray-300 h-[75.76vh] drop-shadow-md m-[1.5vw]'></div>
-        <div className='w-[28.1vw] bg-gray-300 h-[75.76vh] drop-shadow-md m-[1.5vw]'></div>
-        <div className='w-[28.1vw] bg-gray-300 h-[75.76vh] drop-shadow-md m-[1.5vw]'></div>
-        <div className='w-[28.1vw] bg-gray-300 h-[75.76vh] drop-shadow-md m-[1.5vw]'></div>
-        <div className='w-[28.1vw] bg-gray-300 h-[75.76vh] drop-shadow-md m-[1.5vw]'></div>
+        <div className='w-[28.1vw] text-[#333333] h-[79vh] shadow-lg m-[1.5vw]'>
+          <div className='h-3/5 overflow-hidden '><img className='hover:scale-110 transition-all duration-300' src='/testimonial1.jpg' alt='card-image'/></div>
+          <div className='h-2/5  mx-[2vw]'>
+            <h1 className='text-[1.7vw] font-bold'>Nunc sagittis bibendum elit</h1>
+            <p className='py-[1vw]'>Lorem ipsum amet consectetur adipisicing elit.ratione quis et culpa repellendus odit doloremque modi quibusdam.</p>
+            <button className='text-white bg-[#a58838] py-[0.8vw] px-[1.75vw] hover:bg-[#333333] transition-all duration-300 cursor-pointer'>READ MORE</button>
+          </div>
+        </div>
+        <div className='w-[28.1vw] text-[#333333] h-[79vh] shadow-lg m-[1.5vw]'>
+          <div className='h-3/5 overflow-hidden '><img className='hover:scale-110 transition-all duration-300' src='/testimonial1.jpg' alt='card-image'/></div>
+          <div className='h-2/5  mx-[2vw]'>
+            <h1 className='text-[1.7vw] font-bold'>Nunc sagittis bibendum elit</h1>
+            <p className='py-[1vw]'>Lorem ipsum amet consectetur adipisicing elit.ratione quis et culpa repellendus odit doloremque modi quibusdam.</p>
+            <button className='text-white bg-[#a58838] py-[0.8vw] px-[1.75vw] hover:bg-[#333333] transition-all duration-300 cursor-pointer'>READ MORE</button>
+          </div>
+        </div>
+        <div className='w-[28.1vw] text-[#333333] h-[79vh] shadow-lg m-[1.5vw]'>
+          <div className='h-3/5 overflow-hidden '><img className='hover:scale-110 transition-all duration-300' src='/testimonial1.jpg' alt='card-image'/></div>
+          <div className='h-2/5  mx-[2vw]'>
+            <h1 className='text-[1.7vw] font-bold'>Nunc sagittis bibendum elit</h1>
+            <p className='py-[1vw]'>Lorem ipsum amet consectetur adipisicing elit.ratione quis et culpa repellendus odit doloremque modi quibusdam.</p>
+            <button className='text-white bg-[#a58838] py-[0.8vw] px-[1.75vw] hover:bg-[#333333] transition-all duration-300 cursor-pointer'>READ MORE</button>
+          </div>
+        </div>
+        <div className='w-[28.1vw] text-[#333333] h-[79vh] shadow-lg m-[1.5vw]'>
+          <div className='h-3/5 overflow-hidden '><img className='hover:scale-110 transition-all duration-300' src='/testimonial1.jpg' alt='card-image'/></div>
+          <div className='h-2/5  mx-[2vw]'>
+            <h1 className='text-[1.7vw] font-bold'>Nunc sagittis bibendum elit</h1>
+            <p className='py-[1vw]'>Lorem ipsum amet consectetur adipisicing elit.ratione quis et culpa repellendus odit doloremque modi quibusdam.</p>
+            <button className='text-white bg-[#a58838] py-[0.8vw] px-[1.75vw] hover:bg-[#333333] transition-all duration-300 cursor-pointer'>READ MORE</button>
+          </div>
+        </div>
+        <div className='w-[28.1vw] text-[#333333] h-[79vh] shadow-lg m-[1.5vw]'>
+          <div className='h-3/5 overflow-hidden '><img className='hover:scale-110 transition-all duration-300' src='/testimonial1.jpg' alt='card-image'/></div>
+          <div className='h-2/5  mx-[2vw]'>
+            <h1 className='text-[1.7vw] font-bold'>Nunc sagittis bibendum elit</h1>
+            <p className='py-[1vw]'>Lorem ipsum amet consectetur adipisicing elit.ratione quis et culpa repellendus odit doloremque modi quibusdam.</p>
+            <button className='text-white bg-[#a58838] py-[0.8vw] px-[1.75vw] hover:bg-[#333333] transition-all duration-300 cursor-pointer'>READ MORE</button>
+          </div>
+        </div>
+        <div className='w-[28.1vw] text-[#333333] h-[79vh] shadow-lg m-[1.5vw]'>
+          <div className='h-3/5 overflow-hidden '><img className='hover:scale-110 transition-all duration-300' src='/testimonial1.jpg' alt='card-image'/></div>
+          <div className='h-2/5  mx-[2vw]'>
+            <h1 className='text-[1.7vw] font-bold'>Nunc sagittis bibendum elit</h1>
+            <p className='py-[1vw]'>Lorem ipsum amet consectetur adipisicing elit.ratione quis et culpa repellendus odit doloremque modi quibusdam.</p>
+            <button className='text-white bg-[#a58838] py-[0.8vw] px-[1.75vw] hover:bg-[#333333] transition-all duration-300 cursor-pointer'>READ MORE</button>
+          </div>
+        </div>
       </div>
     </div>
   )
