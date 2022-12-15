8528e591587e9993883bbd059474d0c5c0f8e3df
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-12-15 11:07:40 +0530

    wrong import

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import Banner from '../components/banner'
+import Banner from '../components/Banner'
 
 const about = () => {
   return (
