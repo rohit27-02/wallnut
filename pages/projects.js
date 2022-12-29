0f1aefa9777e0ac24ee84fbc9992205e9964d89a
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-12-29 20:35:57 +0530

    c

--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,6 @@ const projects = () => {
           <p className='text-[4.85vh] md:text-[3.875rem] flex md:flex-col pr-0 md:pr-[50px] mr-[3.9vh] md:mr-[50px]  items-center md:items-start gap-[2vh] md:gap-0  md:border-r-[0.15vw] border-[#a58838] font-bold'><span className='text-[#a58838] text-[2.5vh] md:text-2xl italic'>LATEST</span>PROJECTS</p>
           <p className='font-semibold text-[1.5vh] md:text-sm'>“Independence can be tough. Without a studio to back you up, when you finish a feature and want to start a new project you have to start from zero.”</p>
         </div>
-
         <div className='h-[630px] overflow-hidden mb-[50px]'>
           <ResponsiveMasonry
             columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}
@@ -34,6 +33,7 @@ const projects = () => {
             </Masonry>
           </ResponsiveMasonry>
         </div>
+        
     </div>
   )
 }
