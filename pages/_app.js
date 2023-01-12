6cf6c0f9e0e3a835c53b467283367064ed36b24a
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2023-01-12 11:42:58 +0530

    c

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,21 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { HashLoader } from 'react-spinners'
 function MyApp({ Component, pageProps }) {
   const [loaded, setloaded] = useState(false);
   useEffect(() => {
-   setloaded(true)
-  }, [pageProps,Component]);
+    if (typeof window != "undefined") {
+      document.addEventListener("load",setloaded(true))
+    }
+  }, []);
   return <>
-  {loaded ? <div><Navbar/>
-  <Component {...pageProps} />
-  <Footer/>
-  </div>:<div className='absolute top-[50%] left-[50%] '><HashLoader color="#a58838" /></div>}
-  </> 
+    {loaded ? <div><Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div> : <div className='absolute top-[50%] left-[50%] '><HashLoader color="#a58838" /></div>}
+  </>
 }
 
 export default MyApp
